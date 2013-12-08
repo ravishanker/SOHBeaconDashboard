be14db966306387ae36bd328ad18512e7968297c
Sushant Verma <sushant.40@gmail.com>
2013-12-08 11:08:18 +1100

    dynamic user data from beaconstore

--- WebContent/sections/users.jsp
+++ WebContent/sections/users.jsp
@@ -1,4 +1,5 @@
 
+<%@page import="com.soh.sohbeacon.store.BeaconStore"%>
 <h2 id="users">Users</h2>
 
 <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&libraries=visualization"></script>
@@ -26,15 +27,22 @@
 
 //Adding 500 Data Points
 var map, pointarray, heatmap;
-
 var taxiData = [
+
+<%for (String user : BeaconStore.getInstance().usersFromBeacon("beacon1"))
+ { %>
 new google.maps.LatLng(-33.857821, 151.214194),
+<%}%>
+
+<%for (String user : BeaconStore.getInstance().usersFromBeacon("beacon2"))
+{ %>
 new google.maps.LatLng(-33.856252, 151.215192),
-new google.maps.LatLng(-33.856252, 151.215192),
-new google.maps.LatLng(-33.856252, 151.215192),
-new google.maps.LatLng(-33.856252, 151.215192),
-new google.maps.LatLng(-33.856252, 151.215192),
-new google.maps.LatLng(-33.857286, 151.215428)
+<%}%>
+
+<%for (String user : BeaconStore.getInstance().usersFromBeacon("beacon3"))
+{ %>
+new google.maps.LatLng(-33.857286, 151.215428),
+<%}%>
 ];
 
 function initialize() {
@@ -79,10 +87,6 @@ var gradient = [
  'rgba(255, 0, 0, 1)'
 ]
 heatmap.set('gradient', heatmap.get('gradient') ? null : gradient);
-}
-
-function changeRadius() {
-heatmap.set('radius', heatmap.get('radius') ? null : 20);
 }
 
 function changeOpacity() {
--- src/com/soh/sohbeacon/store/BeaconStore.java
+++ src/com/soh/sohbeacon/store/BeaconStore.java
@@ -1,6 +1,8 @@
 package com.soh.sohbeacon.store;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 
 import com.soh.sohbeacon.model.Beacon;
 
@@ -11,11 +13,17 @@ public class BeaconStore {
 	
 	private ArrayList<Beacon> notificationBeacon = new ArrayList<Beacon>();
 	
+	private HashMap<String, HashSet<String>> userTracking = new HashMap<String, HashSet<String>>();
+	
 	public static BeaconStore getInstance() {
 		return _instance;
 	}
 
 	private BeaconStore() {
+		addUserToBeacon("1", "beacon1");
+		addUserToBeacon("2", "beacon2");
+		addUserToBeacon("1", "beacon2");
+		addUserToBeacon("1", "beacon3");
 	}
 
 	public void addNotificationBeacon(Beacon newBeacon) {
@@ -50,5 +58,31 @@ public class BeaconStore {
 			}
 		}
 		return null;
+	}
+	
+	public void addUserToBeacon(String userId, String beacon){
+		if (userTracking.containsKey(beacon) == false)
+		{
+			userTracking.put(beacon, new HashSet<String>());
+		}
+		userTracking.get(beacon).add(userId);
+	}
+	
+	public void removeUserFromBeacon(String userId, String beacon){
+		try {
+			userTracking.get(beacon).remove(userId);
+			
+		}catch (Exception e)
+		{
+			
+		}
+	}
+	
+	public HashSet<String> usersFromBeacon(String beacon){
+		if (userTracking.containsKey(beacon))
+		{
+			return userTracking.get(beacon);
+		}
+		return new HashSet<String>();
 	}
 }
